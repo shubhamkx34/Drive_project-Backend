e6d7ebe643b262a6b2ddcef0087c1a5c17dbbe62
Shubham <shubhamkx34@gmail.com>
2026-06-13 16:13:28 +0530

    Configured login interface and integrated user routing logic

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
 const express = require("express");
 const userRouter = require("./routes/user.routes")
+const cookieParser = require("cookie-parser"); 
+const indexRouter= require('./routes/index.routes')
 
 const dotenv = require("dotenv")
 dotenv.config()
-const connectToDB = require('./config/db')
+const connectToDB = require('./config/db');
+
 connectToDB()
 
 const app = express();
 app.set("view engine", "ejs");
 app.use(express.json());   //to solve undefined console output when using post method
 app.use(express.urlencoded({ extended: true }));   //to solve undefined console output when using post method
+app.use(cookieParser())
 
-app.use('/user',userRouter)
+app.use('/',indexRouter)         //Login & authentication page
+app.use('/user',userRouter)     //register page
+
 
 app.listen(3000, () => {
   console.log("Express running on 3000 port");  //callback
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const { body, validationResult } = require("express-validator");
-const bcrypt = require('bcrypt')
+const bcrypt = require("bcrypt");
 const userModal = require("../models/user");
-const jwt = require("jsonwebtoken")
+const jwt = require("jsonwebtoken");
+
 
 /*FORM RELATED FRONTEND-SERVER INTERACTION  */
 //To render register.ejs to frontend from server
@@ -40,15 +41,17 @@ router.post(
 );
 
 /*LOGIN AND AUTHENTICATION RELATED FRONTEND-SERVER INTERACTION  */
-//To render login.ejs to frontend from server
-router.get('/login', (req,res) => {
-  res.render('login')
-})
 
+//To render login.ejs to frontend from server
+
+router.get("/login", (req, res) => {
+  res.render("login");
+});
 
 //To get data from frontend to server
-router.post('/login',
-  body("password").trim().isLength({ min: 6}),
+router.post(
+  "/login",
+  body("password").trim().isLength({ min: 6 }),
   body("username").trim().isLength({ min: 3 }),
   async (req, res) => {
     const errors = validationResult(req);
@@ -59,29 +62,44 @@ router.post('/login',
         message: "Invalid data",
       });
     }
-    //Now checking the database of the user who is trying to login ,if it is saved or not as a registered user 
-    const {username,password}=req.body;
+
+    //Now checking the database of the user who is trying to login ,if it is saved or not as a registered user
+
+    const { username, password } = req.body;
 
     const user = await userModal.findOne({
-      username:username
-    })
-    if(!user){
+      username: username,
+    });
+    if (!user) {
       return res.status(400).json({
-       message:'Username or Password is incorrect'
+        message: "Username or Password is incorrect", //checking username if it matches or not
       });
-
-      const ismatch = await bcrypt.compare(password,user.password)
-      if(!ismatch){
-        return res.status(400).json({
-          message: 'username or password is incorrect'
-        })
-      }
-
     }
 
+    const ismatch = await bcrypt.compare(password, user.password); //checking req.body password and stored password in database
+    if (!ismatch) {
+      return res.status(400).json({
+        message: "username or password is incorrect",
+      });
+    }
 
-  })
+    //Jsonwebtoken
+    //If password and username both matches , then token is generated and stored in form of cookies in the browser ,which helps to know if a user is logged in or not
+    const token = jwt.sign(
+      {
+        userId: user._id, //1st parameter are all the fields in these token that are called payloads
+        email: user.email,
+        username: user.username,
+      },
+      process.env.JWT_SECRET, //2nd parameter is secret key stored on .env secure  file
+    );
 
 
+    //Token created , now storing it on cookies
+
+    res.cookie('token',token) 
+    res.send('Logged in')   //'token is the name of cookie and value is token here 
+  },
+);
 
 module.exports = router;
